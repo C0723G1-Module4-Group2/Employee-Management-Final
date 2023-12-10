fa2f2827da27ccdce8e49c8917aed2b0efa8d0a8
minhthuan196 <minhthuandtu@gmail.com>
2023-12-10 12:37:22 +0700

    thuan update final

--- Employee-Management-Casestudy/src/main/java/com/example/employeemanagementcasestudy/controller/TeachingScheduleController.java
+++ Employee-Management-Casestudy/src/main/java/com/example/employeemanagementcasestudy/controller/TeachingScheduleController.java
@@ -1,12 +1,14 @@
 package com.example.employeemanagementcasestudy.controller;
 import com.example.employeemanagementcasestudy.model.TeachingSchedule;
 import com.example.employeemanagementcasestudy.service.ITeachingScheduleService;
+import com.example.employeemanagementcasestudy.service.IUserRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -14,6 +16,8 @@ import java.util.List;
 public class TeachingScheduleController {
     @Autowired
     private ITeachingScheduleService teachingScheduleService;
+    @Autowired
+    private IUserRoleService userRoleService;
     @GetMapping("")
     public String showCalendar(Model model){
         List<TeachingSchedule> teachingSchedules = teachingScheduleService.getAll();
@@ -21,9 +25,15 @@ public class TeachingScheduleController {
         return "teaching_schedule/list";
     }
     @GetMapping("/user")
-    public String showCalendarForUser(Model model){
+    public String showCalendarForUser(Model model, Principal principal){
 //        List<TeachingSchedule> teachingSchedules = teachingScheduleService.getAll();
 //        model.addAttribute("listSchedule",teachingSchedules);
+        if (principal == null) {
+            return "redirect:/login";
+        }
+        if (userRoleService.isUserAdmin(principal.getName())) {
+            return "redirect:/403";
+        }
         return "teaching_schedule/listForTeacher";
     }
 }
